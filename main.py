35033bba7b46c6bd9fb91b7c912d9d40d8624942
j3iiifn <j3iiifn@gmail.com>
2019-08-18 11:46:36 +0900

    Add function to search MAC address and port

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import socket
 import time
 import math
 
+import bluetooth
+
 import wattchecker
 from data_manager import DataManager
 
@@ -19,16 +21,52 @@ def get_logger():
     from logging import basicConfig, getLogger, DEBUG
     return getLogger(__name__)
 
-def main():
-    configure_logging()
-    logger = get_logger()
+configure_logging()
+logger = get_logger()
 
+def _get_macaddr():
+    """Discover WATT CHECKER and get MAC address of it
+
+    Return:
+        string: a mac address of WATT CHECKER
+    """
+    logger.info('Discovering WATT CHECKER...')
+    nearby_devices = bluetooth.discover_devices(lookup_names=True)
+    for addr, name in nearby_devices:
+        if name == 'WATT CHECKER':
+            return addr
+
+    raise Exception('Error: Cannot find MAC address of WATT CHECKER')
+
+def _get_port(mac_address):
+    """Get port number of RFCOMM protocol
+    * Prerequisites: paired to WATT CHECKER with bluetoothctl command.
+    
+    Args:
+        mac_address (string): a MAC address of WATT CHECKER
+
+    Return:
+        int: a port number of RFCOMM protocol
+    """
+    logger.info('Searching RFCOMM service...')
+    services = bluetooth.find_service(address=mac_address)
+    for s in services:
+        if s['protocol'] == 'RFCOMM':
+            return s['port']
+
+    raise Exception('Error: Cannot find RFCOMM protocol.')
+
+def search_wattchecker():
+    macaddr = _get_macaddr()
+    port = _get_port(macaddr)
+    return macaddr, port
+
+def main():
     data_manager = DataManager(buff_size=180, output=['csv'])
 
-    serverMACAddress = 'WATTCHECKER_MAC_ADDRESS'
-    port = 6
+    mac_address, port = search_wattchecker()
     s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-    s.connect((serverMACAddress, port))
+    s.connect((mac_address, port))
 
     try:
         logger.info('Initializing...')
